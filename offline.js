4ff96db212a0ea101e7c0d0ad887f5d02b7d8d88
Querino Rafael Oliveira <querinorafael.oliveira@gmail.com>
2025-04-30 09:35:10 -0300

    2ª tentativa de exportação

---
+++ offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1746014121,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/asteróide1-sheet0.png",
+		"images/asteróide2-sheet0.png",
+		"images/asteróide3-sheet0.png",
+		"media/título.ogg",
+		"media/gamemusic.ogg",
+		"media/25. victory loop.ogg",
+		"media/starting_themoon.ogg",
+		"media/themoon.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
